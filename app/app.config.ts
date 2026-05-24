1b701e4f6816584577c32f24cc98aebddb840ae8
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-05-24 21:03:50 +0200

    fix: prefer aside navigation for operating manual

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,9 +2,6 @@ export default defineAppConfig({
   header: {
     title: 'happydesigns docs',
   },
-  navigation: {
-    sub: 'header',
-  },
   seo: {
     title: 'happydesigns docs',
     description: 'The central operating manual for the happydesigns ecosystem.',
